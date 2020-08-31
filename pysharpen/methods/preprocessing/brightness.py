d32fb92860b726bcc2c198c6136178412a6e3fa8
trekin <a.trekin@skoltech.ru>
2020-08-31 10:57:08 +0300

    WIP brightness

--- pysharpen/methods/preprocessing/brightness.py
+++ pysharpen/methods/preprocessing/brightness.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import sqrt
 from pysharpen.methods.img_proc import ImgProc
 from pysharpen.functional import linear_brightness_scale
 
@@ -12,6 +13,7 @@ class LinearBrightnessScale(ImgProc):
     def __init__(self, method='minmax', dtype=None,
                  per_channel=False, pan_separate=False,
                  process_pan=True, process_ms=True,
+                 std_width=3.0,
                  **kwargs):
         """
 
@@ -32,6 +34,8 @@ class LinearBrightnessScale(ImgProc):
         self.per_channel = per_channel
         # if ms is scaled per-channel, we cannot scale pan together with all the channels
         self.pan_separate = pan_separate or per_channel
+        #Valid only for method = 'meanstd'
+        self.std_width = std_width
 
         # separate pan mins and maxs for pan and ms
         self._pan_mins = []
@@ -47,6 +51,17 @@ class LinearBrightnessScale(ImgProc):
         else:
             self._ms_min_value = 0
             self._ms_max_value = 255
+
+        # intermediate values for the mean-std variant
+        self._pan_means = []
+        self._pan_stds = []
+        self._pan_mes = []
+        self._pan_nums = []
+
+        self._ms_means = []
+        self._ms_stds = []
+        self._ms_mes = []
+        self._ms_nums = []
 
         self.setup_required = True
 
@@ -149,30 +164,61 @@ class LinearBrightnessScale(ImgProc):
 
         return mean, std, num, me
 
+    def _totalmeanstd(self, means, stds, nums, mes):
+        mean = np.sum([m*n for m,n in zip(means, nums)])/np.sum(nums)
+        # TODO: test it
+        std =sqrt(np.sum(
+            [(nums[i]/np.sum(nums)*(stds[i]**2 + nums[i]*(mean - means[i])**2 + 2*(means[i] - mean)*mes[i]))
+               for i in range(len(means))]))
+        return mean, std
+
     def _meanstd_from_patch(self, pan, ms):
+
+        # We also need min and max values if we use meanstd
+        self._minmax_from_patch(pan, ms)
+
         if not self.pan_separate:
             mean, std, num, me = self._meanstdme(np.concatenate(ms, pan, axis=0))
-            self._ms_means.append(mean)
             self._pan_means.append(mean)
-            self._ms_stds.append(std)
             self._pan_stds.append(std)
-            self._imsizes.append(num)
-            self._ms_mes.append(me)
+            self._pan_nums.append(num)
             self._pan_mes.append(me)
+
+            self._ms_nums.append(num)
+            self._ms_mes.append(me)
+            self._ms_means.append(mean)
+            self._ms_stds.append(std)
         else:
             mean, std, num, me = self._meanstdme(pan)
             self._pan_means.append(mean)
             self._pan_stds.append(std)
             self._pan_mes.append(me)
-            self._imsizes.append(num)
+            self._pan_nums.append(num)
 
-            if self.per_channel:
-                mean, std, num, me = self._meanstdme(ms)
-                self._ms_means.append(mean)
-                self._ms_stds.append(std)
-                self._ms_mes.append(me)
-
-        return
+            axis = 1 if self.per_channel else None
+            mean, std, num, me = self._meanstdme(ms, axis=axis)
+            self._ms_means.append(mean)
+            self._ms_stds.append(std)
+            self._ms_mes.append(me)
+            self._ms_nums.append(num)
 
     def _finalize_meanstd(self):
-        return
+        # We need all the statistics to be calculated for every patch in the same order, so we check the length
+        patches_num = set(len(stat) for stat in [self._ms_means, self._ms_stds, self._ms_nums, self._ms_mes,
+                      self._pan_means, self._pan_stds, self._pan_nums, self._pan_mes])
+        if len(patches_num)!= 1:
+            raise ValueError('Some of the patches staticstics were not calculated correcty, '
+                             'and the length of the patch statistics arrays are different')
+        ms_mean, ms_std = self._totalmeanstd(self._ms_means, self._ms_stds, self._ms_nums, self._ms_mes)
+        pan_mean, pan_std = self._totalmeanstd(self._pan_means, self._pan_stds, self._pan_nums, self._pan_mes)
+        # We find the actual min and max of images
+        self._finalize_minmax()
+
+        # Then we make the min higher and the max lower if it is necessary
+        # Maybe not use minmax, and just clip to the value range?
+        axis = 1 if self.per_channel else None
+        self._pan_min_value = max(pan_mean - self.std_width*pan_std, self._pan_min_value)
+        self._ms_min_value = np.max([ms_mean - self.std_width*ms_std, self._ms_min_value], axis=axis)
+        self._pan_max_value = min(pan_mean + self.std_width*pan_std, self._pan_max_value)
+        self._ms_max_value = np.min([ms_mean + self.std_width*ms_std,self._ms_max_value], axis=axis )
+
